e52a678581ee1f98b55c58d1127b9eb6c4624b4c
liewshin <58600282+liewshin@users.noreply.github.com>
2020-06-20 14:16:43 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+#Retrieving Data
+if (!file.exists(FileName)){
 FileData <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-FileLocation <- "Assn3.zip"
-download.file(FileData, FileLocation, method = "curl")
+FileName <- "Assn3.zip"
+download.file(FileData, FileName, method = "curl")
+unzip(FileName)
+}
