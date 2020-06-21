cb17abdf7607f598a699dc72da3318b49db19aec
liewshin <58600282+liewshin@users.noreply.github.com>
2020-06-21 12:45:01 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -36,28 +36,28 @@ mergedtest <- cbind(subjecttest, xtest, ytest)
 mergedall <- rbind(mergedtrain, mergedtest)
 
 #Extracting Mean and Standard Deviation
-ExtractedData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+extracteddata <- mergedall %>% select(subject, code, contains("mean"), contains("std"))
 
 #Naming Activities
-ExtractedData$code <- activities[TidyData$code, 2]
+extracteddata$code <- activities[extracteddata$code, 2]
 
 #Labelling Dataset
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+names(extracteddata)[2] = "activity"
+names(extracteddata) <- gsub("Acc", "Accelerometer", names(extracteddata))
+names(extracteddata) <- gsub("Gyro", "Gyroscope", names(extracteddata))
+names(extracteddata) < -gsub("BodyBody", "Body", names(extracteddata))
+names(extracteddata) <- gsub("Mag", "Magnitude", names(extracteddata))
+names(extracteddata) <- gsub("^t", "Time", names(extracteddata))
+names(extracteddata) <- gsub("^f", "Frequency", names(extracteddata))
+names(extracteddata) <- gsub("tBody", "TimeBody", names(extracteddata))
+names(extracteddata) <- gsub("-mean()", "Mean", names(extracteddata), ignore.case = TRUE)
+names(extracteddata) <- gsub("-std()", "STD", names(extracteddata), ignore.case = TRUE)
+names(extracteddata) <- gsub("-freq()", "Frequency", names(extracteddata), ignore.case = TRUE)
+names(extracteddata) <- gsub("angle", "Angle", names(extracteddata))
+names(extracteddata) <- gsub("gravity", "Gravity", names(extracteddata))
 
 #Second Independant Dataset
-FinalData <- TidyData %>%
+finaldata <- extracteddata %>%
     group_by(subject, activity) %>%
     summarise_all(funs(mean))
-write.table(FinalData, "FinalData.txt", row.name=FALSE)
+write.table(finaldata, "FinalData.txt", row.name=FALSE)
